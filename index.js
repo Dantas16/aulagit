7b12dacf46da647763ee151cd3b4391670f5c82f
Cloud <jvhc3002@gmail.com>
2022-08-15 18:07:03 -0300

    corrigindo commit

--- index.js
+++ index.js
@@ -6,11 +6,7 @@ window.addEventListener('DOMContentLoaded', function() {
   hello();
 })
 
-<<<<<<< HEAD
 function now(){
 
 }
-=======
-
 var jonathas = 1000
->>>>>>> 014925df4a1a156f36c8d6275b7358a0a7f83212
